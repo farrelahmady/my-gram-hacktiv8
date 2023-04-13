835477adc3e2882560b2fc857f587c63cadb5fe4
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2023-04-13 12:23:24 +0700

    authentication middleware

---
+++ middlewares/authentication.go
@@ -0,0 +1,25 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/farrelahmady/my-gram-hacktiv8/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Auth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		verifyToken, err := helpers.VerifyToken(c)
+		_ = verifyToken
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error":   "Unauthorized",
+				"message": err.Error(),
+			})
+			return
+		}
+
+		c.Set("user", verifyToken)
+		c.Next()
+	}
+}
