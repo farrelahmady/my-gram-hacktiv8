90b98e00138c1cfb76ad4ff70b578aa9354cbe0b
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2023-04-13 11:11:27 +0700

    jwt helpers

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"flag"
-	"log"
-	"strings"
 
 	"github.com/farrelahmady/my-gram-hacktiv8/database"
-	"github.com/farrelahmady/my-gram-hacktiv8/migrations"
 )
 
 func main() {
@@ -18,20 +15,5 @@ func main() {
 func HandleArgs() {
 	flag.Parse()
 	args := flag.Args()
-	for _, arg := range args {
-		command := strings.Split(arg, "=")[0]
-		var params []string
-		if len(strings.Split(arg, "=")) > 1 {
-			params = strings.Split(strings.Split(arg, "=")[1], ",")
-		}
-		// log.Println(command, params)
-		switch command {
-		case "migrate":
-			db := database.GetDB()
-			migrations.Execute(db, params...)
-		case "seed":
-			log.Println("seed")
-		}
-
-	}
+	_ = args
 }
